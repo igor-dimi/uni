713990097fd8059da613c20252ef33ad95bb09b9
Igor Dimitrov <igor-dimi@protonmail.com>
2024-10-14 17:34:17 +0200

    added ws24/25 page and other minor changes

---
+++ ws24-25/week-sum-1.sh
@@ -0,0 +1,4 @@
+for i in $(seq 15);
+do
+    echo -e "## Week $i\n\n* **date**: \n\n"
+done
---
+++ ws24-25/week-sum.sh
@@ -0,0 +1,4 @@
+for i in $(seq 15);
+do
+    echo -e "## Week $i\n\n### Lecture 1\n\n* **date**: \n\n### Lecture 2\n\n* **date**: \n"
+done
